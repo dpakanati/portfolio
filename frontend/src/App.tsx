fa5ea946e342ded6e6a95b620ae73993dcd2f342
DhanushP <dhanushpakanati@gmail.com>
2023-10-11 20:13:39 -0500

    routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,28 @@
 import './App.css'
+import NavtoolBar from "./Components/Navbar/NavtoolBar.tsx";
+import {useEffect, useState} from "react";
 
 function App() {
-
+  const[darkMode, setDarkMode]  =useState(false);
+  useEffect(()=>{
+      if(darkMode){
+          document.documentElement.classList.add('dark');
+      }
+      else{
+          document.documentElement.classList.remove('dark');
+      }
+  },[darkMode])
   return (
-    <h1 className="text-3xl font-bold underline">
-        Hello World!
-    </h1>
+      <div className="h-screen w-full bg-white dark:bg-gray-800">
+          <button
+              onClick={() => setDarkMode(!darkMode)}
+              className="p-2 m-4 bg-blue-500 text-white dark:bg-blue-300 dark:text-gray-800"
+          >
+              Toggle Dark Mode
+          </button>
+          {<NavtoolBar/>}
+      </div>
+
   )
 }
 
