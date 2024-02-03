f50dfd578561eaf77a2dcdd49c3ee38b98b90d57
Dhanush <dhanushpakanati@gmail.com>
2024-02-03 01:29:54 -0600

    Added header with all styling.
    Todo: underline the selected header element

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,27 @@
 import './App.css'
+import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
 
 function App() {
-
-  return (
-    <h1 className="text-3xl font-bold underline">
-        Hello World!
-    </h1>
-  )
+    // const sections =[
+    //     {id:'home', title: "Home"},
+    //     {id:'about', title: "About"},
+    //     {id:'contact', title: "Contact"},
+    //     {id:'projects', title: "Projects"},
+    // ];
+    return(
+        <div>
+            <Home/>
+        </div>
+        // <div className = "App">
+        //     <Header sections = {sections}/>
+        //     <main>
+        //         {sections.map((section) => (
+        //             <Section key={section.id} id={section.id} title={section.title}  />
+        //         ))}
+        //     </main>
+        // </div>
+    );
 }
 
-export default App
+export default App;
---
+++ frontend/src/Components/Home/Home.tsx
@@ -0,0 +1,27 @@
+import React, {useState} from "react";
+import Header from "../Header/Header";
+import "./Home.css";
+const Home = () =>{
+    // const [showHeader, setShowHeader] = useState(true);
+    // const [startY, setStartY] = useState(0);
+    // const handleTouchStart = (e)=>{
+    //     setStartY(e.touches[0].clientY);
+    // };
+    // const handleTouchMove = (e) =>{
+    //     const currentY = e.touches[0].clientY;
+    //     const deltaY = currentY - startY;
+    //     if(deltaY>10){
+    //         setShowHeader(true);
+    //     } else if (deltaY< -10){
+    //         setShowHeader(false);
+    //     }
+    // };
+    return(
+        <div className = "Home">
+            <Header/>
+
+        </div>
+    );
+};
+
+export default  Home
