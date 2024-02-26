f64dd2cb8e2d7a393d48c976cc40d34e930a9bc0
Dhanush <dhanushpakanati@gmail.com>
2024-02-26 17:11:59 -0600

    minor changes

--- frontend/src/Components/Cards/ScrollableCards.tsx
+++ frontend/src/Components/Cards/ScrollableCards.tsx
@@ -1,28 +1,29 @@
 import './ScrollableCards.css';
+
 export function ScrollableCards() {
     return (
         <div className="d-flex scrollable-cards">
-            <div className = "col-6">
-                <div className = "card m-2" style={{width: "18rem"}}>
-                    <img src="public/assets/card1.jpg" className="card-img-top" alt="Card 1"/>
+            <div className="col-6">
+                <div className="card m-2" style={{width: "18rem"}}>
+                    <img src="assets/card1.jpg" className="card-img-top" alt="Card 1"/>
                     <div className="card-body">
                         <h5 className="card-title">Card 1</h5>
                         <p className="card-text">Some quick example text </p>
                     </div>
                 </div>
             </div>
-            <div className = "col-6">
-                <div className = "card m-2" style={{width: "18rem"}}>
-                    <img src="public/assets/card1.jpg" className="card-img-top" alt="Card 2"/>
+            <div className="col-6">
+                <div className="card m-2" style={{width: "18rem"}}>
+                    <img src="assets/card1.jpg" className="card-img-top" alt="Card 2"/>
                     <div className="card-body">
                         <h5 className="card-title">Card 1</h5>
                         <p className="card-text">Some quick example text </p>
                     </div>
                 </div>
             </div>
-            <div className = "col-6">
-                <div className = "card m-2" style={{width: "18rem"}}>
-                    <img src="public/assets/card1.jpg" className="card-img-top" alt="Card 3"/>
+            <div className="col-6">
+                <div className="card m-2" style={{width: "18rem"}}>
+                    <img src="assets/card1.jpg" className="card-img-top" alt="Card 3"/>
                     <div className="card-body">
                         <h5 className="card-title">Card 1</h5>
                         <p className="card-text">Some quick example text.</p>
@@ -32,4 +33,5 @@ export function ScrollableCards() {
         </div>
     );
 }
+
 export default ScrollableCards;
--- frontend/src/Components/ProfilePicture/ProfilePics.tsx
+++ frontend/src/Components/ProfilePicture/ProfilePics.tsx
@@ -3,7 +3,7 @@ import './ProfilePics.css';
 export function ProfilePics() {
     return (
         <div className="d-flex justify-content-center align-items-center custom-height">
-            <img src="public/assets/images/dhanush.JPG" className="w-50 h-50 rounded-circle custom_img "/>
+            <img src="assets/images/dhanush.JPG" className="w-50 h-50 rounded-circle custom_img "/>
         </div>
     )
 }
