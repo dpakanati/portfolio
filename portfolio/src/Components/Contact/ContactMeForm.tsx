4e71f810975157246323c466b7e4701730a4f491
Dhanush <dhanushpakanati@gmail.com>
2024-03-25 01:21:21 -0500

    handled static form submission in contact me page

--- portfolio/src/Components/Contact/ContactMeForm.tsx
+++ portfolio/src/Components/Contact/ContactMeForm.tsx
@@ -4,16 +4,15 @@ function ContactMeForm() {
     return (
         <div className="pt-5">
             <h1 className="pb-3 text-start">Say Hello!</h1>
-            <form action="mailto:your-email@example.com" method="post" encType="text/plain">
+            <form action="https://formspree.io/f/mleqwqng" method="POST" encType="multipart/form-data">
                 <div className="mb-3">
-                    <input type="text" placeholder="Name.." className="form-control" name="name"/>
+                    <input type="text" placeholder="Name.." className="form-control" name="username"/>
                 </div>
                 <div className="mb-3 ">
                     <input type="email" placeholder="Email Address.." className="form-control" name="email"/>
                 </div>
                 <div className="mb-3">
-
-                    <textarea className="form-control" placeholder="Type message.." rows="8" name="message"></textarea>
+                    <textarea className="form-control" placeholder="Type message.." rows="8" name="message"/>
                 </div>
                 <div className="text-start">
                     <button type="submit" className="btn btn-primary">Submit</button>
